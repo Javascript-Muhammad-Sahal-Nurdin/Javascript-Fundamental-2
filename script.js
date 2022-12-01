cdc0ad3095ec250828b12c66c35d35de4dfd8f45
hp <sahalnurdin@gmail.com>
2022-12-01 17:26:30 +0700

    add changes

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function calcAge1(birthYear) {
 }
 // Namun tidak bisa dengan Functions Expression. Jadi untuk penggunaannya kita lebih ke fuction expressions. sama saja lebih ke preferensi masing-masing
 
-*/
+
 // Arrow Function
 // Arrow function digunakan sebagai mempersingkat penulisan fuction
 
@@ -172,4 +172,123 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
+
+
+// Introduction Array
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+// Bundle variabel di atas menjadi array (big container)
+// Deklarasi array secara umum
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// Deklarasi Array juga bisa seperti berikut
+const years1 = new Array(1991, 1984, 2000, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+// Length ini akan mengambil banyak elemen di dalam array, dihitung dari 1
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // Kenapa -1 karena array di sini bukan zero based untuk menghitung banyaknay length
+// array tidak bisa menerima statement di dalam kurung
+
+friends[2] = 'Jay';
+console.log(friends); // array bisa diubah dan bukan immutable
+// Bisa di mutate
+// friends = ['Bob', 'Alice'] // error kalo di deklarasikan ke constant
+
+// di js array dapat berisi macam2 tipe data
+const firstName = 'Sahal';
+const sahal = [firstName, 'Nurdin', 2037 - 2003, 'Student', friends];
+console.log(sahal);
+console.log(sahal.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+console.log(calcAge(years)); // Hasilnya adalah NaN maka array tidak bisa dioperasikan seperti ini, bisanya satu-satu
+
+// Berikutnya adalah caranya
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]); // INi akan memilih elemen terakhir
+
+console.log(age1, age2, age3);
+
+// Ini akan work
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+console.log(ages);
+
+
+// Basic Array Operations (Methods)
+
+// Add elements
+const friends = ['Michael', 'Steven', 'Peter'];
+friends.push('Jay') // Push adalah Function yang dipanggil untuk menambahkan elemen
+console.log(friends);
+// Untuk mencari length
+const newLength = friends.push('Jay');
+console.log(newLength);
+
+friends.unshift('John'); // Menambahkan elemen menjadi yang pertama
+console.log(friends);
+
+// Remove Elements
+friends.pop(); // Menghapus element terakhir
+const popped = friends.pop() // akan menampilkan elemen yang dihapus
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // Akan menghapus elemen pertama
+console.log(friends);
+
+// Ini akan mencari indeks
+console.log(friends.indexOf('steven'));
+console.log(friends.indexOf('Bob')); // output akan -1 karena error
+
+// Inludes hasilnya akan boolean // Strict equality jadi akan dicheck tipe datanya juga
+friends.push(23)
+console.log(friends.includes('Bob'));
+console.log(friends.includes('Steven'));
+console.log(friends.includes('23'));
+console.log(friends.includes(23));
+
+// Cara pakai include
+if (friends.includes('Peter')) {
+    console.log('You have a friend called Steven');
+} 
+*/
+
+// Coding Challenge # 2
+// Tip Calculator
+
+const calcTip = (billValue) => {
+    if (billValue >= 50 && billValue <= 300) {
+        return billValue * 0.15;
+    } else {
+        return billValue * 0.2;
+        
+    }
+
+    // Bisa juga menggunakan ternary operator
+}
+calcTip(100);
+
+const bills = [123, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+// bonus
+const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]]
+console.log(total);
+
+
 
