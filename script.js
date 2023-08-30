52e1adfc87d797c1f2f63316eee05e54da267a0a
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-08-30 16:50:41 +0700

    Add changes

--- script.js
+++ script.js
@@ -43,6 +43,40 @@
 /////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////
+// LECTURE: Functions
+// 1. Write a function called 'describeCountry' which takes three parameters:
+// 'country', 'population' and 'capitalCity'. Based on this input, the
+// function returns a string with this format: 'Finland has 6 million people and its
+// capital city is Helsinki'
+// 2. Call this function 3 times, with input data for 3 different countries. Store the
+// returned values in 3 different variables, and log them to the console
+function describeCountry(country, population, capitalCity) {
+  const describe = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+  return describe;
+}
+
+const country1 = describeCountry(
+  prompt("Masukkan nama country: "),
+  prompt("Masukkan banyak population: "),
+  prompt("Masukkan nama capital city: ")
+);
+const country2 = describeCountry(
+  prompt("Masukkan nama country: "),
+  prompt("Masukkan banyak population: "),
+  prompt("Masukkan nama capital city: ")
+);
+const country3 = describeCountry(
+  prompt("Masukkan nama country: "),
+  prompt("Masukkan banyak population: "),
+  prompt("Masukkan nama capital city: ")
+);
+console.log(country1);
+console.log(country2);
+console.log(country3);
+
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////
 // Functions Declarations vs Expressions
 
 // Function Declarations
